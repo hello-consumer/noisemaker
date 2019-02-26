a4fef6da0c92ca9fad23f0d0a5354a711dbc7687
Joe Johnson <joe.wendell.johnson@gmail.com>
2019-02-26 11:06:57 -0600

    Added touch event support

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ for(var i = 0; i < keys.length; i++){
     keys[i].setAttribute("data-pitch", 440 * Math.pow(2, (i/keys.length)));
     keys[i].addEventListener('mousedown', toggleOscillatorOn);
     keys[i].addEventListener('mouseup', toggleOscillatorOff);
+    keys[i].addEventListener('touchstart', toggleOscillatorOn);
+    keys[i].addEventListener('touchend', toggleOscillatorOff);
 }
 
 function initialize(){
